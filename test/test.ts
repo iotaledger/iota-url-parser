4914314d8e3c9ea166734fe99b28bc12010f9c7b
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-05-20 15:05:26 -0500

    chore: add basic Jest testing to library

---
+++ test/test.ts
@@ -0,0 +1,7 @@
+describe('File: test.ts', () => {
+    describe('Function: test', () => {
+        it('should test properly', () => {
+            expect(1 + 1).toEqual(1 + 1)
+        })
+    })
+})
