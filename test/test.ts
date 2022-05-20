9b1ab417c3aea90ad52f31d4769b31c541a14998
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-05-20 15:25:04 -0500

    chore: use testing from non index file

--- test/test.ts
+++
@@ -1,7 +0,0 @@
-describe('File: test.ts', () => {
-    describe('Function: test', () => {
-        it('should test properly', () => {
-            expect(1 + 1).toEqual(1 + 1)
-        })
-    })
-})
